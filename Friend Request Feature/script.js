fadaf7a38e04edcc3155387a297b78fdd1f1e09c
Debaditya <debadityasingh9@gmail.com>
2023-09-16 13:20:04 +0530

    Using Single Button

--- Friend Request Feature/script.js
+++ Friend Request Feature/script.js
@@ -1,14 +1,24 @@
 var istatus = document.querySelector("h5");
-
+var button = document.querySelector("button");
 var add = document.querySelector("#add");
-var removeFriend = document.querySelector("#msg");
+// var removeFriend = document.querySelector("#msg");
+var flag = 0;
 
 add.addEventListener("click", function(){
-    istatus.innerHTML = "Friends";
-    istatus.style.color = "green";
+    if(flag == 0){
+        istatus.innerHTML = "Friends";
+        istatus.style.color = "green";
+        button.innerHTML = "Remove";
+        flag = 1;
+    } else {
+        istatus.innerHTML = "Stranger";
+        istatus.style.color = "Red";
+        button.innerHTML = "Add Friend";
+        flag = 0;
+    }
 })
 
-removeFriend.addEventListener("click", function(){
-    istatus.innerHTML = "Stranger";
-    istatus.style.color = "Red";
-})
+// removeFriend.addEventListener("click", function(){
+//     istatus.innerHTML = "Stranger";
+//     istatus.style.color = "Red";
+// })
