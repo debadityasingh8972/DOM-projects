432f186ab43ba2fbf914ba054a69c374b96f35f0
Debaditya <debadityasingh9@gmail.com>
2023-09-16 20:58:40 +0530

    functionality added

---
+++ Insta Like Feature/script.js
@@ -0,0 +1,10 @@
+var con = document.querySelector('#container')
+var icon = document.querySelector("i")
+
+con.addEventListener("dblclick", function(){
+    icon.style.transform = 'translate(-50%,-50%) scale(1)';
+    setTimeout(function(){
+        icon.style.transform = 'translate(-50%,-50%) scale(0)';
+    },1000);
+})
+
